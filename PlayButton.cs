99681fd9515f7f752641aae4aa348550f83dee92
SmileyFace799 <Henrikgran85@gmail.com>
2024-12-08 04:36:10 +0100

    Added barebones main menu screen

---
+++ PlayButton.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class PlayButton : Button {
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) {
+	}
+
+	public void OnPressed() {
+		GetTree().ChangeSceneToFile("res://Board.tscn");
+	}
+}
